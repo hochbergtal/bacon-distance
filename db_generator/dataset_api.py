fb1313c4b16a3c1ec3149f280608322e3559b7f1
Tal <hochbergtal@gmail.com>
2026-03-15 13:35:23 +0200

    make dataset api for the imdb tsv files

---
+++ db_generator/dataset_api.py
@@ -0,0 +1,37 @@
+import csv
+from dataclasses import dataclass
+from typing import Generator
+
+
+@dataclass
+class TitleActor:
+    title_id: str
+    actor_id: str
+
+
+@dataclass
+class IdName:
+    id: str
+    name: str
+
+
+def iter_title_actors(title_principals_filename: str) -> Generator[TitleActor]:
+    with open(title_principals_filename, "r") as tsvfile:
+        principals_reader = csv.reader(tsvfile, delimiter="\t")
+        for row in principals_reader:
+            if row[3] == "actor":
+                yield TitleActor(row[0], row[2])
+
+
+def iter_title_names(title_basics_filename: str) -> Generator[IdName]:
+    with open(title_basics_filename, "r") as tsvfile:
+        basics_reader = csv.reader(tsvfile, delimiter="\t")
+        for row in basics_reader:
+            yield IdName(row[0], row[2])
+
+
+def iter_actor_names(name_basics_filename: str) -> Generator[IdName]:
+    with open(name_basics_filename, "r") as tsvfile:
+        basics_reader = csv.reader(tsvfile, delimiter="\t")
+        for row in basics_reader:
+            yield IdName(row[0], row[1])

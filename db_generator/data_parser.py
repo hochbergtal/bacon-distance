0022e87f3048847a921e07a84373d49fe5ac21ce
Tal <hochbergtal@gmail.com>
2026-03-15 15:50:19 +0200

    add docs

--- db_generator/data_parser.py
+++ db_generator/data_parser.py
@@ -6,23 +6,36 @@ from dataset_api import iter_title_actors, iter_title_names, iter_actor_names
 
 @dataclass
 class TitleData:
+    """Entry for title's name and actors list"""
+
     actors: List[str]
     name: str = ""
 
 
 @dataclass
 class ActorData:
+    """Entry for actor's name and movie they're featured in"""
+
     titles: List[str]
     name: str = ""
 
 
 @dataclass
 class TitleActorsData:
+    """A json-like dataclass of all titles with their names and actors, and actors with their names and featured titles"""
+
     titles: Dict[str, TitleData]
     actors: Dict[str, ActorData]
 
 
 class DataParser:
+    """A class for creating, filling and dumping the database.
+
+    :param title_principals: path to the file that connects actors and titles IDs
+    :param title_basics: path to the file that titles' IDs and names
+    :param name_basics: path to the file that actors' IDs and names
+    """
+
     def __init__(
         self, title_principals: str, title_basics: str, name_basics: str
     ) -> None:
@@ -32,6 +45,7 @@ class DataParser:
         self.name_basics = name_basics
 
     def fill_ids(self) -> None:
+        """Fill the titles' and actors' IDs from the given files to the data."""
         for title_actor in iter_title_actors(self.title_principals):
             if title_actor.title_id in self.data.titles:
                 self.data.titles[title_actor.title_id].actors.append(
@@ -52,6 +66,7 @@ class DataParser:
                 )
 
     def fill_names(self) -> None:
+        """Fill the titles' and actors' names from the given files to the data."""
         for title_name in iter_title_names(self.title_basics):
             self.data.titles[title_name.id].name = title_name.name
 
@@ -59,5 +74,9 @@ class DataParser:
             self.data.actors[actor_name.id].name = actor_name.name
 
     def dump_to_file(self, path: str) -> None:
+        """Dump the saved data to the given file in a JSON format.
+
+        :param path: the path of the file
+        """
         with open(path, "w") as dump_file:
             json.dump(asdict(self.data), dump_file)
--- db_generator/dataset_api.py
+++ db_generator/dataset_api.py
@@ -5,12 +5,16 @@ from typing import Generator
 
 @dataclass
 class TitleActor:
+    """Entry for actor who played in a title"""
+
     title_id: str
     actor_id: str
 
 
 @dataclass
 class IdName:
+    """Entry for actor/title name and its ID"""
+
     id: str
     name: str
 
@@ -18,8 +22,14 @@ class IdName:
 def iter_title_actors(
     title_principals_filename: str,
 ) -> Generator[TitleActor, None, None]:
+    """Iterates over all title-actor entries in a dataset file.
+
+    :param title_principals_filename: a tsv file of titles and their principals as presented in imdb
+    :yields: an entry for an actor who played in a title
+    """
     with open(title_principals_filename, "r") as tsvfile:
         principals_reader = csv.reader(tsvfile, delimiter="\t")
+        # skip fields names row
         next(principals_reader)
         for row in principals_reader:
             if row[3] == "actor":
@@ -27,16 +37,28 @@ def iter_title_actors(
 
 
 def iter_title_names(title_basics_filename: str) -> Generator[IdName, None, None]:
+    """Iterates over all title-name entries in a dataset file.
+
+    :param title_basics_filename: a tsv file of titles and their basic info as presented in imdb
+    :yields: an entry for a title ID and name
+    """
     with open(title_basics_filename, "r") as tsvfile:
         basics_reader = csv.reader(tsvfile, delimiter="\t")
+        # skip fields names row
         next(basics_reader)
         for row in basics_reader:
             yield IdName(row[0], row[2])
 
 
 def iter_actor_names(name_basics_filename: str) -> Generator[IdName, None, None]:
+    """Iterates over all actor-name entries in a dataset file.
+
+    :param name_basics_filename: a tsv file of people's names and their basic info as presented in imdb
+    :yields: an entry for an actor ID and name
+    """
     with open(name_basics_filename, "r") as tsvfile:
         basics_reader = csv.reader(tsvfile, delimiter="\t")
+        # skip fields names row
         next(basics_reader)
         for row in basics_reader:
             yield IdName(row[0], row[1])

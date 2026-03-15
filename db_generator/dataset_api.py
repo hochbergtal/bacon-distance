9b56b13f510b7345ee44d9c491a9cff114f945d1
Tal <hochbergtal@gmail.com>
2026-03-15 15:28:58 +0200

    fix generator type hint

--- db_generator/dataset_api.py
+++ db_generator/dataset_api.py
@@ -15,7 +15,9 @@ class IdName:
     name: str
 
 
-def iter_title_actors(title_principals_filename: str) -> Generator[TitleActor]:
+def iter_title_actors(
+    title_principals_filename: str,
+) -> Generator[TitleActor, None, None]:
     with open(title_principals_filename, "r") as tsvfile:
         principals_reader = csv.reader(tsvfile, delimiter="\t")
         for row in principals_reader:
@@ -23,14 +25,14 @@ def iter_title_actors(title_principals_filename: str) -> Generator[TitleActor]:
                 yield TitleActor(row[0], row[2])
 
 
-def iter_title_names(title_basics_filename: str) -> Generator[IdName]:
+def iter_title_names(title_basics_filename: str) -> Generator[IdName, None, None]:
     with open(title_basics_filename, "r") as tsvfile:
         basics_reader = csv.reader(tsvfile, delimiter="\t")
         for row in basics_reader:
             yield IdName(row[0], row[2])
 
 
-def iter_actor_names(name_basics_filename: str) -> Generator[IdName]:
+def iter_actor_names(name_basics_filename: str) -> Generator[IdName, None, None]:
     with open(name_basics_filename, "r") as tsvfile:
         basics_reader = csv.reader(tsvfile, delimiter="\t")
         for row in basics_reader:

a4a09aeeac1f3c36a7b6b2b415b131178ee1f37f
Tal <hochbergtal@gmail.com>
2026-03-16 14:41:42 +0200

    remove logs

--- db_generator/generate_db.py
+++ db_generator/generate_db.py
@@ -23,25 +23,9 @@ def main() -> None:
     with DataParser(
         result_file_path, TITLE_PRINCIPALS_NAME, TITLE_BASICS_NAME, NAME_BASICS_NAME
     ) as parser:
-        print("DB generation started.")
-
-        print("filling titles and actors ID...")
-        start = time.time()
         parser.fill_ids()
-        end = time.time()
-        print(f"Took {end - start} seconds")
-
-        print("filling title names...")
-        start = end
         parser.fill_title_names()
-        end = time.time()
-        print(f"Took {end - start} seconds")
-
-        print("filling actor names...")
-        start = end
         parser.fill_actor_names()
-        end = time.time()
-        print(f"Took {end - start} seconds")
 
 
 if __name__ == "__main__":
